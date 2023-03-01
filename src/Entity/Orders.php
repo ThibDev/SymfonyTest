19c9c612a9ba2859a6554e51e9c7af863a9d4a4e
Thib Dev <thibaultdevinstep@gmail.com>
2023-03-01 13:23:57 +0100

    test symfony Entity

---
+++ src/Entity/Orders.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersRepository::class)]
+class Orders
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20, unique: true)]
+    private ?string $reference = null;
+
+    #[ORM\Column (options: ['default' => 'CURRENT_TIMESTAMP'])]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    private ?Coupons $coupons = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $users = null;
+
+    #[ORM\OneToMany(mappedBy: 'orders', targetEntity: OdersDetails::class, orphanRemoval: true)]
+    private Collection $odersDetails;
+
+    public function __construct()
+    {
+        $this->odersDetails = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getCoupons(): ?Coupons
+    {
+        return $this->coupons;
+    }
+
+    public function setCoupons(?Coupons $coupons): self
+    {
+        $this->coupons = $coupons;
+
+        return $this;
+    }
+
+    public function getUsers(): ?User
+    {
+        return $this->users;
+    }
+
+    public function setUsers(?User $users): self
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, OdersDetails>
+     */
+    public function getOdersDetails(): Collection
+    {
+        return $this->odersDetails;
+    }
+
+    public function addOdersDetail(OdersDetails $odersDetail): self
+    {
+        if (!$this->odersDetails->contains($odersDetail)) {
+            $this->odersDetails->add($odersDetail);
+            $odersDetail->setOrders($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOdersDetail(OdersDetails $odersDetail): self
+    {
+        if ($this->odersDetails->removeElement($odersDetail)) {
+            // set the owning side to null (unless already changed)
+            if ($odersDetail->getOrders() === $this) {
+                $odersDetail->setOrders(null);
+            }
+        }
+
+        return $this;
+    }
+}
