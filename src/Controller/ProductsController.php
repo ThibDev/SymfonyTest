47d5e8b2d0a18fd2ce9c3ae9ff7af85789e9ddd7
Thib Dev <thibaultdevinstep@gmail.com>
2023-03-03 16:25:35 +0100

    add admin, profile, products controllers and templates

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/products', name: 'app_products')]
+    public function index(): Response
+    {
+        return $this->render('products/index.html.twig', [
+            'controller_name' => 'ProductsController',
+        ]);
+    }
+
+    #[Route('/products/{slug}', name: 'app_details')]
+    public function details(Product $product): Response
+    {
+        
+        return $this->render('products/details.html.twig', compact('product'));
+    }
+}
