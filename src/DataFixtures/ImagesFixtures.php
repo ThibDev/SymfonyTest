130792515e40ed7f1f5f239ef0755709d04fbf3b
Thib Dev <thibaultdevinstep@gmail.com>
2023-03-03 14:18:27 +0100

    add Fixtures2

--- src/DataFixtures/ImagesFixtures.php
+++ src/DataFixtures/ImagesFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Images;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
-class ImagesFixtures extends Fixture
+class ImagesFixtures extends Fixture implements DependentFixtureInterface
 {
+
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($img=1; $img < 15; $img++) { 
+
+            $image = new Images();
+            $image->setName($faker->image(null, 640, 480));
+            $product = $this->getReference('prod-'.rand(1, 9));
+            $image->setProducts($product);
+            $manager->persist($image);
+         
+        }
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ProductFixtures::class
+        ];
     }
 }
